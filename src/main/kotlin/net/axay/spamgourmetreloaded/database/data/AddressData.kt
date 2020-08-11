2472408171cbb7d7c6ebe2057426e56e3b8669c3
Jakob K <bluefireolymp@gmail.com>
2020-08-11 16:55:56 +0200

    Added BounceAddressData

--- src/main/kotlin/net/axay/spamgourmetreloaded/database/data/AddressData.kt
+++ src/main/kotlin/net/axay/spamgourmetreloaded/database/data/AddressData.kt
@@ -5,10 +5,10 @@ import com.fasterxml.jackson.annotation.JsonInclude
 @JsonInclude(JsonInclude.Include.NON_NULL)
 data class UserAddressData(
     val address: String,
-    val usesLeft: Int
-/*    ,
-    val trustedSenders: Collection<String>?,
-    val alternativeForwardTo: String?*/ // TODO implement these features later
+    val usesLeft: Int,
+    val ifOnlyTrusted: Boolean? = null,
+    val trustedSenders: Collection<String>? = null,
+    val alternativeForwardTo: String? = null
 )
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -16,5 +16,11 @@ data class AnswerAddressData(
     val address: String,
     val answerAsAddress: String,
     val answerToAddress: String,
-    val forAddress: String
+    val forUser: String
+)
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class BounceAddressData(
+    val address: String,
+    val informUser: String
 )
