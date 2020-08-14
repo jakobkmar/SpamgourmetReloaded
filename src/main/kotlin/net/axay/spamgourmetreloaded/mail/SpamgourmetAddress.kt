1d80f73c43754b4ccee2302e89ea8b7b04df4b19
Jakob K <bluefireolymp@gmail.com>
2020-08-14 16:27:56 +0200

    Update SpamgourmetAddress.kt

--- src/main/kotlin/net/axay/spamgourmetreloaded/mail/SpamgourmetAddress.kt
+++ src/main/kotlin/net/axay/spamgourmetreloaded/mail/SpamgourmetAddress.kt
@@ -3,9 +3,14 @@
 package net.axay.spamgourmetreloaded.mail
 
 import net.axay.spamgourmetreloaded.main.Manager
+import net.axay.spamgourmetreloaded.main.ValueHolder
 import org.subethamail.smtp.util.EmailUtils
 
 class SpamgourmetAddress(val fullAddress: String) {
+
+    constructor(address: String, isOnlyFirstPart: Boolean) : this(kotlin.run {
+        if (isOnlyFirstPart) address else "$address@${Manager.configManager.mainConfig.domainConfig.mainAddressDomain}"
+    })
 
     val splitParts by lazy { fullAddress.split('@') }
     val firstPart by lazy { splitParts[0] }
@@ -14,17 +19,22 @@ class SpamgourmetAddress(val fullAddress: String) {
     val firstPartValues by lazy { firstPart.split('.') }
 
     val isValid by lazy { splitParts.size == 2 && EmailUtils.isValidEmailAddress(fullAddress) }
-    val isSpamgourmetAddress by lazy { secondPart == Manager.configManager.mainConfig.addressDomain }
+    val isSpamgourmetAddress by lazy { secondPart == Manager.configManager.mainConfig.domainConfig.mainAddressDomain }
 
     val type by lazy {
 
         when (firstPartValues.size) {
-            3 -> SpamgourmetAddressType.SPAMGOURMET_USER_ADDRESS
+            3 -> {
+                when {
+                    firstPartValues[1].toIntOrNull() != null -> SpamgourmetAddressType.SPAMGOURMET_USER_ADDRESS
+                    else -> SpamgourmetAddressType.UNKNOWN
+                }
+            }
             2 -> {
                 when(firstPartValues.last()) {
-                    "answer" -> SpamgourmetAddressType.SPAMGOURMET_ANSWER_ADDRESS
-                    "bounce-s" -> SpamgourmetAddressType.SPAMGOURMET_SPAM_BOUNCE_ADDRESS
-                    "bounce-a" -> SpamgourmetAddressType.SPAMGOURMET_ANSWER_BOUNCE_ADDRESS
+                    ValueHolder.ANSWER_ADDRESS_KEY -> SpamgourmetAddressType.SPAMGOURMET_ANSWER_ADDRESS
+                    ValueHolder.SPAM_BOUNCE_ADDRESS_KEY -> SpamgourmetAddressType.SPAMGOURMET_SPAM_BOUNCE_ADDRESS
+                    ValueHolder.ANSWER_BOUNCE_ADDRESS_KEY -> SpamgourmetAddressType.SPAMGOURMET_ANSWER_BOUNCE_ADDRESS
                     else -> SpamgourmetAddressType.UNKNOWN
                 }
             }
