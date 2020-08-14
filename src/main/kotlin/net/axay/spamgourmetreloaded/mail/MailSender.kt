06b969403a5db8556d78d5e3825bc01701c52d60
Jakob K <bluefireolymp@gmail.com>
2020-08-14 16:28:07 +0200

    Update MailSender.kt

--- src/main/kotlin/net/axay/spamgourmetreloaded/mail/MailSender.kt
+++ src/main/kotlin/net/axay/spamgourmetreloaded/mail/MailSender.kt
@@ -1,5 +1,6 @@
 package net.axay.spamgourmetreloaded.mail
 
+import net.axay.spamgourmetreloaded.main.Manager
 import org.simplejavamail.api.email.Email
 import org.simplejavamail.mailer.MailerBuilder
 
@@ -7,6 +8,12 @@ object MailSender {
 
     private val mailer = MailerBuilder
         .withSMTPServer("smtp.mailtrap.io", 2525, "7e207e8e6fcb79", "4163009e830a42")
+        .withSMTPServer(
+                Manager.configManager.mainConfig.smtpLoginInformation.host,
+                Manager.configManager.mainConfig.smtpLoginInformation.port,
+                Manager.configManager.mainConfig.smtpLoginInformation.username,
+                Manager.configManager.mainConfig.smtpLoginInformation.password
+        )
         .clearEmailAddressCriteria()
         .buildMailer()
 
