0e733a4090e2cf3b2c2e69f060e4497c99c0a419
bluefireoly <bluefireolymp@gmail.com>
2020-08-16 18:06:48 +0200

    Removed duplicates

--- src/main/kotlin/net/axay/spamgourmetreloaded/mail/SpamgourmetAddressGenerator.kt
+++ src/main/kotlin/net/axay/spamgourmetreloaded/mail/SpamgourmetAddressGenerator.kt
@@ -1,82 +1,96 @@
 package net.axay.spamgourmetreloaded.mail
 
+import com.mongodb.client.MongoCollection
 import net.axay.spamgourmetreloaded.database.contains
 import net.axay.spamgourmetreloaded.database.data.AnswerAddressData
 import net.axay.spamgourmetreloaded.database.data.BounceAddressData
 import net.axay.spamgourmetreloaded.main.Manager
 import net.axay.spamgourmetreloaded.main.ValueHolder
+import org.bson.conversions.Bson
 import org.litote.kmongo.and
 import org.litote.kmongo.eq
 import org.litote.kmongo.findOne
-import java.math.BigInteger
 import java.security.MessageDigest
 import javax.xml.bind.DatatypeConverter
-import kotlin.random.Random
-import kotlin.random.nextLong
 
 object SpamgourmetAddressGenerator {
 
     fun generateAnswerAddress(username: String, answerAsAddress: String, answerToAddress: String, alternativeAllowed: String?): String {
 
-        Manager.dataManager.answerAddressCollection.findOne(
-                and(
-                        AnswerAddressData::forUser eq username,
-                        AnswerAddressData::answerAsAddress eq answerAsAddress,
-                        AnswerAddressData::answerToAddress eq answerToAddress
-                )
-        )?.let { return "${it.address}.${ValueHolder.ANSWER_ADDRESS_KEY}" }
-
         return "${
-            generateKeyFromStringWhile(username + answerToAddress) {
-                Manager.dataManager.answerAddressCollection.contains(AnswerAddressData::address eq it)
-            }.apply { 
-                Manager.dataManager.answerAddressCollection.insertOne(AnswerAddressData(
-                        this, answerAsAddress, answerToAddress, username, alternativeAllowed?.let { listOf(it) }
-                ))
-            }
+            findKeyOrGenerateNewOneFromStringWhile(
+                    username + answerToAddress,
+                    Manager.dataManager.answerAddressCollection,
+                    and(
+                            AnswerAddressData::forUser eq username,
+                            AnswerAddressData::answerAsAddress eq answerAsAddress,
+                            AnswerAddressData::answerToAddress eq answerToAddress
+                    ),
+                    { "${it.address}.${ValueHolder.ANSWER_ADDRESS_KEY}" },
+                    { AnswerAddressData::address eq it },
+                    { key -> AnswerAddressData(key, answerAsAddress, answerToAddress, username, alternativeAllowed?.let { listOf(it) }) }
+            )
         }.${ValueHolder.ANSWER_ADDRESS_KEY}"
 
     }
 
     fun generateSpamBounceAddress(username: String, spamgourmetUserAddress: String, userAddress: String): String {
 
-        Manager.dataManager.spamBounceAddressCollection.findOne(
-                and(
-                        BounceAddressData::informUser eq username,
-                        BounceAddressData::forAddress eq spamgourmetUserAddress
-                )
-        )?.let { return "${it.address}.${ValueHolder.SPAM_BOUNCE_ADDRESS_KEY}" }
-
         return "${
-            generateKeyFromStringWhile(username + spamgourmetUserAddress) {
-                Manager.dataManager.spamBounceAddressCollection.contains(BounceAddressData::address eq it)
-            }.apply { 
-                Manager.dataManager.spamBounceAddressCollection.insertOne(BounceAddressData(
-                        this, username, spamgourmetUserAddress, userAddress
-                ))
-            }
+            findKeyOrGenerateNewOneFromStringWhile(
+                    username + spamgourmetUserAddress,
+                    Manager.dataManager.spamBounceAddressCollection,
+                    and(
+                            BounceAddressData::informUser eq username,
+                            BounceAddressData::forAddress eq spamgourmetUserAddress
+                    ),
+                    { "${it.address}.${ValueHolder.SPAM_BOUNCE_ADDRESS_KEY}" },
+                    { BounceAddressData::address eq it },
+                    { BounceAddressData(it, username, spamgourmetUserAddress, userAddress) }
+            )
         }.${ValueHolder.SPAM_BOUNCE_ADDRESS_KEY}"
 
     }
 
     fun generateAnswerBounceAddress(username: String, spammerAddress: String): String {
 
-        Manager.dataManager.answerBounceAddressCollection.findOne(
-                and(
-                        BounceAddressData::informUser eq username,
-                        BounceAddressData::forAddress eq spammerAddress
-                )
-        )?.let { return "${it.address}.${ValueHolder.ANSWER_BOUNCE_ADDRESS_KEY}" }
-
         return "${
-            generateKeyFromStringWhile(username + spammerAddress) {
-                Manager.dataManager.spamBounceAddressCollection.contains(BounceAddressData::address eq it)
-            }.apply { 
-                Manager.dataManager.answerBounceAddressCollection.insertOne(BounceAddressData(
-                        this, username, spammerAddress, spammerAddress
-                ))
-            }
+            findKeyOrGenerateNewOneFromStringWhile(
+                    username + spammerAddress,
+                    Manager.dataManager.answerBounceAddressCollection,
+                    and(
+                            BounceAddressData::informUser eq username,
+                            BounceAddressData::forAddress eq spammerAddress
+                    ),
+                    { "${it.address}.${ValueHolder.ANSWER_BOUNCE_ADDRESS_KEY}" },
+                    { BounceAddressData::address eq it },
+                    { BounceAddressData(it, username, spammerAddress, spammerAddress) }
+            )
         }.${ValueHolder.ANSWER_BOUNCE_ADDRESS_KEY}"
+
+    }
+
+    private inline fun <E> findKeyOrGenerateNewOneFromStringWhile(
+            string: String,
+            collection: MongoCollection<E>,
+            filter: Bson,
+            crossinline ifFound: (foundObject: E) -> String,
+            crossinline generateWhile: (currentKey: String) -> Bson,
+            crossinline objectToInsert: (key: String) -> E
+    ): String {
+
+        return Manager.dataManager.mongoDB.executeTransaction { session ->
+
+            collection.findOne(session, filter)?.let { return@executeTransaction ifFound.invoke(it) }
+
+            val key = generateKeyFromStringWhile(string) {
+                collection.contains(session, generateWhile.invoke(it))
+            }
+            collection.insertOne(session, objectToInsert.invoke(key))
+
+            return@executeTransaction key
+
+        }
 
     }
 
@@ -88,32 +102,6 @@ object SpamgourmetAddressGenerator {
             key = (key + key.hashCode()).md5()
 
         return key
-
-    }
-
-    // TODO do not use hashCode() <- implementation not consistent
-    // TODO decide for an algorithm to generate answer addresses
-    /**
-     * it has to be:
-     *  - non reversible
-     *  - relatively short
-     *  TODO find out how the original spamgourmet did it
-     */
-    private fun generateKeyFromString(string: String): String {
-
-        val weakSeed = string.hashCode()
-        val weakRandom = Random(weakSeed)
-
-        val strongSeed = kotlin.run {
-            val builder = StringBuilder()
-            while (builder.length < 18)
-                builder.append(Random(string[weakRandom.nextInt(string.length)].hashCode()).nextInt(10).toString())
-            return@run if (builder.length <= 18) builder.toString() else builder.toString().slice(0 until 19)
-        }
-        val strongRandom = Random(strongSeed.toLong())
-
-        val key = strongRandom.nextLong(1000_0000_0000_0000 until 1_0000_0000_0000_0001)
-        return if (key == 1_0000_0000_0000_0000) "0000000000000000" else key.toString()
 
     }
 
