c7e04305ebde665e0ca27faa6703ba356c64b38b
bluefireoly <bluefireolymp@gmail.com>
2020-08-16 18:06:01 +0200

    Made indices unique

--- src/main/kotlin/net/axay/spamgourmetreloaded/database/DataManager.kt
+++ src/main/kotlin/net/axay/spamgourmetreloaded/database/DataManager.kt
@@ -1,5 +1,6 @@
 package net.axay.spamgourmetreloaded.database
 
+import com.mongodb.client.model.IndexOptions
 import net.axay.blueutils.database.mongodb.MongoDB
 import net.axay.spamgourmetreloaded.database.data.AnswerAddressData
 import net.axay.spamgourmetreloaded.database.data.BounceAddressData
@@ -8,29 +9,28 @@ import net.axay.spamgourmetreloaded.database.data.UserData
 import net.axay.spamgourmetreloaded.main.ValueHolder.DATA_PREFIX
 import org.litote.kmongo.descendingIndex
 
-class DataManager(mongoDB: MongoDB) {
+class DataManager(val mongoDB: MongoDB) {
 
     val userCollection = mongoDB.getCollection<UserData>("${DATA_PREFIX}users") {
-        it.createIndex(descendingIndex(UserData::username))
+        it.createIndex(descendingIndex(UserData::username), IndexOptions().unique(true))
     } ?: throw IllegalStateException("The user collection could not be loaded!")
 
 
     val userAddressCollection = mongoDB.getCollection<UserAddressData>("${DATA_PREFIX}user_addresses") {
-        it.createIndex(descendingIndex(UserAddressData::address))
+        it.createIndex(descendingIndex(UserAddressData::address), IndexOptions().unique(true))
     } ?: throw IllegalStateException("The user addresses collection could not be loaded!")
 
     val answerAddressCollection = mongoDB.getCollection<AnswerAddressData>("${DATA_PREFIX}answer_addresses") {
-        it.createIndex(descendingIndex(AnswerAddressData::address))
+        it.createIndex(descendingIndex(AnswerAddressData::address), IndexOptions().unique(true))
     } ?: throw IllegalStateException("The answer addresses collection could not be loaded!")
 
 
     val spamBounceAddressCollection = mongoDB.getCollection<BounceAddressData>("${DATA_PREFIX}spam_bounce_addresses") {
-        it.createIndex(descendingIndex(BounceAddressData::address))
+        it.createIndex(descendingIndex(BounceAddressData::address), IndexOptions().unique(true))
     } ?: throw IllegalStateException("The spam bounce address collection could not be loaded!")
 
     val answerBounceAddressCollection = mongoDB.getCollection<BounceAddressData>("${DATA_PREFIX}answer_bounce_addresses") {
-        it.createIndex(descendingIndex(BounceAddressData::address))
+        it.createIndex(descendingIndex(BounceAddressData::address), IndexOptions().unique(true))
     } ?: throw IllegalStateException("The answer bounce address collection could not be loaded!")
-
 
 }
