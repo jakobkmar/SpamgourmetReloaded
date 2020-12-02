f40a41dbc200d282d652dfdc78d208d4dd5424ca
bluefireoly <bluefireolymp@gmail.com>
2020-12-02 22:30:09 +0100

    Create Database.kt

---
+++ common/src/main/kotlin/net/axay/spamgourmet/database/Database.kt
@@ -0,0 +1,25 @@
+package net.axay.spamgourmet.database
+
+import net.axay.blueutils.database.mongodb.MongoDB
+import net.axay.spamgourmet.data.*
+import net.axay.spamgourmet.main.Values
+
+class Database(mongoDB: MongoDB) {
+
+    val userData =
+        mongoDB.getCollectionOrCreate<UserData>("${Values.PROJECT_PREFIX}user_data")
+
+    val userAddressData =
+        mongoDB.getCollectionOrCreate<UserAddressData>("${Values.PROJECT_PREFIX}user_address_data")
+    val answerAddressData =
+        mongoDB.getCollectionOrCreate<AnswerAddressData>("${Values.PROJECT_PREFIX}answer_address_data")
+
+    val bounceData =
+        mongoDB.getCollectionOrCreate<BounceData>("${Values.PROJECT_PREFIX}bounce_data")
+
+    val answerBounceAddressData =
+        mongoDB.getCollectionOrCreate<AnswerBounceAddressData>("${Values.PROJECT_PREFIX}answer_bounce_address_data")
+    val spamBounceAddressData =
+        mongoDB.getCollectionOrCreate<SpamBounceAddressData>("${Values.PROJECT_PREFIX}spam_bounce_address_data")
+
+}
