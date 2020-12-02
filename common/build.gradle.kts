9d48cf6ed5b33ea35b412fc7672253decc580c7c
bluefireoly <bluefireolymp@gmail.com>
2020-12-02 22:28:32 +0100

    Update build.gradle.kts

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,3 +1,21 @@
 plugins {
+
     `common-build-script-kt`
+
+    kotlin("plugin.serialization") version "1.4.10"
+
+}
+
+dependencies {
+
+    // BLUEUTILS
+    api("net.axay", "BlueUtils", "1.0.1")
+
+    // SERIALIZATION
+    implementation("org.jetbrains.kotlinx", "kotlinx-serialization-json", "1.0.1")
+
+    // KMONGO
+    api("org.litote.kmongo", "kmongo-core", "4.2.0")
+    api("org.litote.kmongo", "kmongo-serialization-mapping", "4.2.0")
+
 }
