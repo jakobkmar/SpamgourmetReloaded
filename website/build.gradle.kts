909584e152b9506d084049748b5d41df7b6cf6e9
bluefireoly <bluefireolymp@gmail.com>
2020-12-04 00:10:37 +0100

    Update build.gradle.kts

--- website/build.gradle.kts
+++ website/build.gradle.kts
@@ -1,3 +1,41 @@
+@file:Suppress("PropertyName")
+
+/*
+ * BUILD CONSTANTS
+ */
+
+val main_class = "net.axay.spamgourmet.website.main.ManagerKt"
+val ktor_version = "1.4.0"
+
+/*
+ * PLUGINS
+ */
+
 plugins {
+
     `common-build-script-kt`
+
+    application
+
 }
+
+/*
+ * DEPENDENCY MANAGEMENT
+ */
+
+dependencies {
+
+    implementation(project(":common"))
+
+    // KTOR
+    compileOnly("io.ktor:ktor-server-netty:$ktor_version")
+
+}
+
+/*
+ * BUILD
+ */
+
+// MAIN CLASS
+
+application.mainClass.set(main_class)
