9cc8cd3489b02f2bc1a08dc159119898c39c4ce7
bluefireoly <bluefireolymp@gmail.com>
2020-12-03 23:48:32 +0100

    Update Logging.kt

--- common/src/main/kotlin/net/axay/spamgourmet/common/logging/Logging.kt
+++ common/src/main/kotlin/net/axay/spamgourmet/common/logging/Logging.kt
@@ -1,25 +1,13 @@
 package net.axay.spamgourmet.common.logging
 
-import net.axay.spamgourmet.mailserver.main.ValueHolder
 import java.io.PrintStream
-import java.lang.StringBuilder
 
-fun logInfo(message: Any?, stream: PrintStream = System.out) {
-    stream.println("${ValueHolder.PREFIX}$message")
+fun log(message: Any?, stream: PrintStream = System.out, prefix: String = "") {
+    stream.println("$prefix >> $message")
 }
 
-fun logInfoMultiline(vararg messageLines: Any?) {
-    val builder = StringBuilder("${ValueHolder.PREFIX}:")
-    messageLines.forEach {
-        var isSecond = false
-        it.toString().split('\n').forEach { innerLine ->
-            if (!isSecond) {
-                builder.append('\n').append(" -> ").append(innerLine)
-                isSecond = true
-            } else {
-                builder.append('\n').append("  |-> ").append(innerLine)
-            }
-        }
-    }
-    println(builder.toString())
-}
+fun logInfo(message: Any?, stream: PrintStream = System.out) =
+    log(message, stream, "INFO")
+
+fun logError(message: Any?, stream: PrintStream = System.err) =
+    log(message, stream, "ERROR")
