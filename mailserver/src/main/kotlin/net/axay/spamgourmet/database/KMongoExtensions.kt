954b278c7a176587739a6fa80ee22ac7efc4ebc7
bluefireoly <bluefireolymp@gmail.com>
2020-12-03 23:21:58 +0100

    Delete KMongoExtensions.kt

--- mailserver/src/main/kotlin/net/axay/spamgourmet/database/KMongoExtensions.kt
+++
@@ -1,9 +0,0 @@
-package net.axay.spamgourmet.database
-
-import com.mongodb.client.ClientSession
-import com.mongodb.client.MongoCollection
-import com.mongodb.client.model.CountOptions
-import org.bson.conversions.Bson
-
-fun <T> MongoCollection<T>.contains(filter: Bson) = countDocuments(filter, CountOptions().limit(1)) == 1L
-fun <T> MongoCollection<T>.contains(clientSession: ClientSession, filter: Bson) = countDocuments(clientSession, filter, CountOptions().limit(1)) == 1L
