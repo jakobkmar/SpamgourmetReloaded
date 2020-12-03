4a560fb110a02914f3e158da6fc22cea615c95d9
bluefireoly <bluefireolymp@gmail.com>
2020-12-03 22:19:02 +0100

    Update BounceData.kt

--- common/src/main/kotlin/net/axay/spamgourmet/data/BounceData.kt
+++ common/src/main/kotlin/net/axay/spamgourmet/data/BounceData.kt
@@ -1,14 +1,17 @@
 package net.axay.spamgourmet.data
 
+import com.github.jershell.kbson.ObjectIdSerializer
+import kotlinx.serialization.Contextual
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import org.bson.BsonObjectId
 import org.litote.kmongo.Id
 import org.litote.kmongo.serialization.InstantSerializer
 import java.time.Instant
 
 @Serializable
 class BounceData(
-    @SerialName("_id") val id: Id<BounceData>,
+    @Contextual @SerialName("_id") val id: Id<BounceData>? = null,
     @Serializable(with=InstantSerializer::class) val time: Instant,
     val type: BounceType,
     val from: String,
