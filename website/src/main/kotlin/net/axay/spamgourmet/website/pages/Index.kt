65d89fcfe0788b2d80c566b9da8e34a919d36b9b
bluefireoly <bluefireolymp@gmail.com>
2020-12-12 02:45:08 +0100

    Update Index.kt

--- website/src/main/kotlin/net/axay/spamgourmet/website/pages/Index.kt
+++ website/src/main/kotlin/net/axay/spamgourmet/website/pages/Index.kt
@@ -4,11 +4,12 @@ import kotlinx.html.*
 import net.axay.spamgourmet.website.pages.common.pageWrapper
 import net.axay.spamgourmet.website.util.svgObject
 
-fun HTML.pageIndex() = pageWrapper("Home") {
+fun HTML.pageIndex() = pageWrapper("Home", "dark_background") {
+
+    svgObject("static/svg/index/topimage.svg", "1067px")
 
 /*    img(src = "static/svg/index/topimage.svg") {
         width = "1067px"
     }*/
-    svgObject("static/svg/index/topimage.svg", "1067px")
 
 }
