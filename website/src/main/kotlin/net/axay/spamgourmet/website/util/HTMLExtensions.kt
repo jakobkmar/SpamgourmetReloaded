91e95f422b3b15c3b9e2d8da87dd10fa1f454dc6
bluefireoly <bluefireolymp@gmail.com>
2020-12-12 23:11:47 +0100

    Update HTMLExtensions.kt

--- website/src/main/kotlin/net/axay/spamgourmet/website/util/HTMLExtensions.kt
+++ website/src/main/kotlin/net/axay/spamgourmet/website/util/HTMLExtensions.kt
@@ -5,10 +5,10 @@ import kotlinx.html.htmlObject
 
 fun FlowOrInteractiveOrPhrasingContent.svgObject(
     src: String,
-    width: String? = null,
-    classes: String? = null
+    classes: String? = null,
+    width: String? = null
 ) = htmlObject(classes) {
     type = "image/svg+xml"
     width?.let { this.width = it }
-    data = "static/svg/index/topimage.svg"
+    data = src
 }
