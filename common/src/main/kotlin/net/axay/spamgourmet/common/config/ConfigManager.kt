c702c817511fc69b4afc5c8034f1d5a7024ec8a8
bluefireoly <bluefireolymp@gmail.com>
2020-12-14 15:34:17 +0100

    Create ConfigManager.kt

---
+++ common/src/main/kotlin/net/axay/spamgourmet/common/config/ConfigManager.kt
@@ -0,0 +1,15 @@
+package net.axay.spamgourmet.common.config
+
+import net.axay.blueutils.database.DatabaseLoginInformation
+import net.axay.blueutils.gson.jsonConfig
+import java.io.File
+
+class ConfigManager(applicationFolder: File) {
+    private val configFolder = File(applicationFolder, "/config/").apply {
+        println(this.absolutePath)
+    }
+
+    val databaseLoginInformation by jsonConfig(File(configFolder, "databaseLoginInformation.json")) {
+        DatabaseLoginInformation.NOTSET_DEFAULT
+    }
+}
