e53920b55a29471ed5e9c92977087bb887d92b99
bluefireoly <bluefireolymp@gmail.com>
2020-12-15 17:54:56 +0100

    Added placeholder authentication

--- website/src/main/kotlin/net/axay/spamgourmet/website/main/KtorManager.kt
+++ website/src/main/kotlin/net/axay/spamgourmet/website/main/KtorManager.kt
@@ -1,12 +1,14 @@
 package net.axay.spamgourmet.website.main
 
 import io.ktor.application.*
+import io.ktor.auth.*
 import io.ktor.html.*
 import io.ktor.http.content.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.sessions.*
 import net.axay.spamgourmet.website.pages.pageIndex
 
 object KtorManager {
@@ -24,7 +26,23 @@ object KtorManager {
 
 }
 
+data class SessionCookie(val username: String)
+
 fun Application.mainModule() {
+
+    install(Authentication) {
+        form("login") {
+            userParamName = "username"
+            passwordParamName = "password"
+            validate { credentials ->
+                UserIdPrincipal(credentials.name)
+            }
+        }
+    }
+
+    install(Sessions) {
+        cookie<SessionCookie>("SESSION")
+    }
 
     routing {
 
@@ -38,6 +56,30 @@ fun Application.mainModule() {
 
         get("/github") {
             call.respondRedirect("https://github.com/bluefireoly/SpamgourmetReloaded")
+        }
+
+        route("/login") {
+            authenticate("login") {
+                post {
+                    call.sessions.set(SessionCookie(
+                        call.principal<UserIdPrincipal>()?.name ?: error("Missing principal")
+                    ))
+                    call.respondRedirect("/logintest")
+                }
+            }
+        }
+
+        get("/logintest") {
+            val session = call.sessions.get<SessionCookie>()
+            if (session != null) {
+                call.respondText("Welcome back, ${session.username}!")
+            } else {
+                call.respondText("You're logged out!")
+            }
+        }
+
+        get("/logout") {
+            call.sessions.clear<SessionCookie>()
         }
 
     }
