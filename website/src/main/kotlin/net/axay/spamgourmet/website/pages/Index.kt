630012bd341dfd6eb62d6c2aed90250d291b7bec
bluefireoly <bluefireolymp@gmail.com>
2020-12-20 00:01:02 +0100

    Update Index.kt

--- website/src/main/kotlin/net/axay/spamgourmet/website/pages/Index.kt
+++ website/src/main/kotlin/net/axay/spamgourmet/website/pages/Index.kt
@@ -5,6 +5,7 @@ import io.ktor.html.*
 import io.ktor.routing.*
 import kotlinx.html.*
 import net.axay.spamgourmet.website.pages.common.pageWrapper
+import net.axay.spamgourmet.website.security.session
 import net.axay.spamgourmet.website.util.svgObject
 
 fun Routing.pageIndex() {
@@ -18,29 +19,40 @@ fun Routing.pageIndex() {
         ) {
 
             div("topview flex_center") {
+
+                val session = call.session
+
                 div("topimage_container") {
                     svgObject("static/svg/index/topimage.svg", "topimage")
                 }
 
                 div("account_panel flex_center") {
                     div("login_panel account_panel_part") {
-                        h1 { +"LOGIN" }
-                        form {
-                            input(name = "username") {
-                                placeholder = "USERNAME"
-                            }
-                            input(name = "password", type = InputType.password) {
-                                placeholder = "PASSWORD"
-                            }
-                            div("flex_right") {
-                                button(type = ButtonType.submit, formMethod = ButtonFormMethod.post) {
-                                    formAction = "/login"
-                                    +"Log in"
+                        if (session == null) {
+
+                            h1 { +"LOGIN" }
+                            form {
+                                input(name = "username") {
+                                    placeholder = "USERNAME"
+                                }
+                                input(name = "password", type = InputType.password) {
+                                    placeholder = "PASSWORD"
+                                }
+                                div("flex_right") {
+                                    button(type = ButtonType.submit, formMethod = ButtonFormMethod.post) {
+                                        formAction = "/login"
+                                        +"Log in"
+                                    }
                                 }
                             }
-                        }
-                        span {
-                            +"You have to log in to access your dashboard!"
+                            span {
+                                +"You have to log in to access your dashboard!"
+                            }
+
+                        } else {
+
+                            h1 { +"Welcome back, ${session.username}!" }
+
                         }
                     }
 
