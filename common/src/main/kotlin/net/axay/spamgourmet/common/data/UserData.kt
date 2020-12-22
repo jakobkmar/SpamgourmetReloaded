98bce74d40b817fe951ed3b6ab7922c26d780857
bluefireoly <bluefireolymp@gmail.com>
2020-12-22 03:41:05 +0100

    Update registration

--- common/src/main/kotlin/net/axay/spamgourmet/common/data/UserData.kt
+++ common/src/main/kotlin/net/axay/spamgourmet/common/data/UserData.kt
@@ -8,7 +8,6 @@ import java.time.Instant
 @Serializable
 class UserData(
     @SerialName("_id") val username: String,
-    val password: ByteArray,
     val realAddress: String,
     val settings: UserSettings = UserSettings(),
     val information: UserInfo
--- website/src/main/kotlin/net/axay/spamgourmet/website/security/Registration.kt
+++ website/src/main/kotlin/net/axay/spamgourmet/website/security/Registration.kt
@@ -2,14 +2,17 @@ package net.axay.spamgourmet.website.security
 
 import at.favre.lib.crypto.bcrypt.BCrypt
 import io.ktor.application.*
+import io.ktor.http.*
 import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.utils.io.core.*
 import kotlinx.serialization.Serializable
 import net.axay.blueutils.database.mongodb.insertOneCatchDuplicate
+import net.axay.spamgourmet.common.data.PasswordAlgorithm
 import net.axay.spamgourmet.common.data.UserData
 import net.axay.spamgourmet.common.data.UserInfo
+import net.axay.spamgourmet.common.data.UserLoginData
 import net.axay.spamgourmet.website.main.db
 import org.litote.kmongo.*
 import java.time.Instant
@@ -31,19 +34,25 @@ fun Routing.registration() = post("/register") {
 
     val hashedPassword = BCrypt.withDefaults().hash(12, postData.password.toCharArray())
 
-    val userData = UserData(
+    val userLoginData = UserLoginData(
         postData.username,
         hashedPassword,
+        PasswordAlgorithm.BCRYPT
+    )
+
+    val userData = UserData(
+        postData.username,
         postData.emailAddress,
         information = UserInfo(joined = Instant.now())
     )
 
-    val insert = db.userData.insertOneCatchDuplicate(userData)
+    val userLoginDataInsert = db.userLoginData.insertOneCatchDuplicate(userLoginData)
+    val userDataInsert = db.userData.insertOneCatchDuplicate(userData)
 
-    if (!insert.wasAcknowledged()) {
-        call.respondText("es gab schon jemanden in der datenbank")
-    } else {
-        call.respondText("jo nun in die datenbank hinzugefügt :poggles:")
-    }
+    if (userLoginDataInsert.wasAcknowledged() && userDataInsert.wasAcknowledged()) {
+
+        call.respond(HttpStatusCode.OK)
+
+    } else call.respond(HttpStatusCode.NotAcceptable)
 
 }
