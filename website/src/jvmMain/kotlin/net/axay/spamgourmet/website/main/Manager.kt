dddedbbf54db19f7f0b93cc62a1bf0b9bbaf0aae
bluefireoly <bluefireolymp@gmail.com>
2020-12-25 21:33:58 +0100

    Update Manager.kt

--- website/src/jvmMain/kotlin/net/axay/spamgourmet/website/main/Manager.kt
+++ website/src/jvmMain/kotlin/net/axay/spamgourmet/website/main/Manager.kt
@@ -3,7 +3,7 @@ package net.axay.spamgourmet.website.main
 import net.axay.blueutils.database.mongodb.MongoDB
 import net.axay.spamgourmet.common.config.ConfigManager
 import net.axay.spamgourmet.common.database.Database
-import net.axay.spamgourmet.common.logging.logInfo
+import net.axay.spamgourmet.common.logging.logMajorInfo
 import java.io.File
 
 val configManager = ConfigManager(File("."))
@@ -12,7 +12,7 @@ val db = Database(mongoDB)
 
 fun main(args: Array<String>) {
 
-    logInfo("Starting webserver...")
+    logMajorInfo("Starting webserver...")
     io.ktor.server.netty.EngineMain.main(args)
 
 }
