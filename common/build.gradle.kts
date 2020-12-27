94cd14f9949b4bf76d5550d5d1250e6817e5b3ed
bluefireoly <bluefireolymp@gmail.com>
2020-12-27 18:46:45 +0100

    Update KMongo

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -18,8 +18,8 @@ kotlin {
                 implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
 
                 // KMONGO
-                api("org.litote.kmongo:kmongo-core:4.2.0")
-                api("org.litote.kmongo:kmongo-serialization-mapping:4.2.0")
+                api("org.litote.kmongo:kmongo-core:4.2.3")
+                api("org.litote.kmongo:kmongo-serialization-mapping:4.2.3")
 
                 // MORDANT
                 api("com.github.ajalt.mordant:mordant:2.0.0-alpha1")
