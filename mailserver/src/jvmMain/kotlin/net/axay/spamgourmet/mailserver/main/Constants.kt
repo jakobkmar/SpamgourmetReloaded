b8f2ed97a87ec5931f58af36c039f8127a6fbe5c
bluefireoly <bluefireolymp@gmail.com>
2021-02-01 00:21:47 +0100

    Update Constants.kt

--- mailserver/src/jvmMain/kotlin/net/axay/spamgourmet/mailserver/main/Constants.kt
+++ mailserver/src/jvmMain/kotlin/net/axay/spamgourmet/mailserver/main/Constants.kt
@@ -1,11 +1,6 @@
 package net.axay.spamgourmet.mailserver.main
 
-import java.util.*
-import javax.mail.Session
-
 object Constants {
-
-    val DEFAULT_MAIL_SESSION: Session = Session.getDefaultInstance(Properties())
 
     const val ANSWER_ADDRESS_KEY = "answer"
     const val SPAM_BOUNCE_ADDRESS_KEY = "bounce-s"
