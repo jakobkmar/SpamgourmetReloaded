80500fa0d3addddb3bdd692af3cd07cc309d2ed2
bluefireoly <bluefireolymp@gmail.com>
2021-02-01 13:46:49 +0100

    Add current thread to log messages

--- common/src/jvmMain/kotlin/net/axay/spamgourmet/common/logging/Logging.kt
+++ common/src/jvmMain/kotlin/net/axay/spamgourmet/common/logging/Logging.kt
@@ -8,7 +8,7 @@ import com.github.ajalt.mordant.terminal.TextColors.*
 val terminal = Terminal()
 
 fun log(message: Any?, prefix: String = "") {
-    terminal.println(black.bg("$prefix ${gray(">>")} $message"))
+    terminal.println(black.bg("$prefix ${gray("(${Thread.currentThread().name}) >>")} $message"))
 }
 
 fun logInfo(message: Any?) =
