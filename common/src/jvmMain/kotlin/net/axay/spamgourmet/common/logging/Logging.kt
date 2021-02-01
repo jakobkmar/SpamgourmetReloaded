866e446e1303225321bd960cadcec5b8d72d0bd4
bluefireoly <bluefireolymp@gmail.com>
2021-02-01 14:27:57 +0100

    Update Logging.kt

--- common/src/jvmMain/kotlin/net/axay/spamgourmet/common/logging/Logging.kt
+++ common/src/jvmMain/kotlin/net/axay/spamgourmet/common/logging/Logging.kt
@@ -2,26 +2,34 @@
 
 package net.axay.spamgourmet.common.logging
 
+import com.github.ajalt.colormath.Color
+import com.github.ajalt.mordant.rendering.TextStyle
 import com.github.ajalt.mordant.terminal.Terminal
 import com.github.ajalt.mordant.terminal.TextColors.*
+import com.github.ajalt.mordant.terminal.TextStyles.bold
 
 val terminal = Terminal()
 
 fun log(message: Any?, prefix: String = "") {
-    terminal.println(("$prefix ${gray("(${Thread.currentThread().name}) >>")} $message"))
+    terminal.println(("$prefix ${
+        terminal.render("(${Thread.currentThread().name}) >>", TextStyle(gray, italic = true))
+    } $message"))
 }
 
+private fun renderMessage(message: Any?, color: Color? = null) =
+    terminal.render(message.toString(), TextStyle(color, bold = true))
+
 fun logInfo(message: Any?) =
-    log((brightWhite)(message.toString()), white("INFO"))
+    log(renderMessage(message), terminal.render("INFO", bold.style))
 
 fun logMajorInfo(message: Any?) =
-    log((brightWhite)(message.toString()), magenta("INFO"))
+    log(renderMessage(message), terminal.render("INFO", TextStyle(magenta, bold = true)))
 
 fun logSuccess(message: Any?) =
-    log((brightWhite)(message.toString()), green("INFO"))
+    log(renderMessage(message), terminal.render("INFO", TextStyle(brightGreen, bold = true)))
 
 fun logWarning(message: Any?) =
-    log((yellow)(message.toString()), brightYellow("WARN"))
+    log(renderMessage(message, yellow), terminal.render("WARN", TextStyle(yellow, bold = true)))
 
 fun logError(message: Any?) =
-    log((red)(message.toString()), brightRed("ERROR"))
+    log(renderMessage(message, red), terminal.render("ERROR", TextStyle(brightRed, bold = true)))
