095452cd42acdefaa6c8c73c43f9454d865a589e
bluefireoly <bluefireolymp@gmail.com>
2021-02-01 13:47:05 +0100

    Use coroutines when reading console input

--- mailserver/src/jvmMain/kotlin/net/axay/spamgourmet/mailserver/console/ConsoleListener.kt
+++ mailserver/src/jvmMain/kotlin/net/axay/spamgourmet/mailserver/console/ConsoleListener.kt
@@ -1,19 +1,22 @@
 package net.axay.spamgourmet.mailserver.console
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import net.axay.spamgourmet.mailserver.main.Manager
-import kotlin.concurrent.thread
 
 object ConsoleListener {
 
     fun listen() {
 
-        thread {
+        GlobalScope.launch(Dispatchers.IO) {
             while (true) {
-                val input = readLine()
-                if (input != null) {
-                    when (input) {
-                        "stop" -> runBlocking { Manager.stop() }
+                val input = readLine() ?: continue
+                when (input) {
+                    "stop" -> {
+                        runBlocking { Manager.stop() }
+                        break
                     }
                 }
             }
