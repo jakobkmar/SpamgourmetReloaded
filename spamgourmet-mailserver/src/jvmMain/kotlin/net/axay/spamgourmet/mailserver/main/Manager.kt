a51fceb6d919bba6bf31eae79c83d46c24b95670
Jakob K <bluefireolymp@gmail.com>
2021-05-13 15:30:13 +0200

    Update Manager.kt

--- spamgourmet-mailserver/src/jvmMain/kotlin/net/axay/spamgourmet/mailserver/main/Manager.kt
+++ spamgourmet-mailserver/src/jvmMain/kotlin/net/axay/spamgourmet/mailserver/main/Manager.kt
@@ -1,15 +1,12 @@
 package net.axay.spamgourmet.mailserver.main
 
 import kotlinx.coroutines.runBlocking
-import net.axay.blueutils.database.mongodb.CoroutineMongoDB
 import net.axay.simplekotlinmail.delivery.MailerManager
 import net.axay.simplekotlinmail.server.smtpServer
 import net.axay.simplekotlinmail.server.start
-import net.axay.spamgourmet.common.database.Database
 import net.axay.spamgourmet.common.logging.logInfo
 import net.axay.spamgourmet.common.logging.logMajorInfo
 import net.axay.spamgourmet.common.logging.logSuccess
-import net.axay.spamgourmet.mailserver.config.MailserverConfigManager
 import net.axay.spamgourmet.mailserver.console.ConsoleListener
 import net.axay.spamgourmet.mailserver.mail.MailerUtils
 import net.axay.spamgourmet.mailserver.mail.SpamgourmetEmail
@@ -19,19 +16,12 @@ fun main() {
     Manager.start()
 }
 
-val db get() = Manager.database
-
 object Manager {
-
     private val smtpServer = smtpServer {
         mailListener { SpamgourmetEmail.process(it) }
     }
 
-    private val mongoDB = CoroutineMongoDB(MailserverConfigManager.databaseLoginInformation)
-    val database = Database(mongoDB)
-
     fun start() {
-
         // add shutdown hook
         Runtime.getRuntime().addShutdownHook(thread(start = false) { stop() })
         // listen to console input
@@ -43,18 +33,14 @@ object Manager {
         smtpServer.start(keepAlive = true)
 
         logSuccess("Started mailserver!")
-
     }
 
     private fun stop() = runBlocking {
-
         logInfo("Stopping mailserver...")
 
         smtpServer.stop()
         MailerManager.shutdownMailers()
 
         logMajorInfo("Mailserver stopped!")
-
     }
-
 }
