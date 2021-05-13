d5b73321ba0f5f434dbf07c795ee4133a9475edc
Jakob K <bluefireolymp@gmail.com>
2021-05-13 15:14:28 +0200

    Update Logging.kt

--- spamgourmet-common/src/jvmMain/kotlin/net/axay/spamgourmet/common/logging/Logging.kt
+++ spamgourmet-common/src/jvmMain/kotlin/net/axay/spamgourmet/common/logging/Logging.kt
@@ -3,48 +3,27 @@
 package net.axay.spamgourmet.common.logging
 
 import com.github.ajalt.colormath.Color
+import com.github.ajalt.mordant.rendering.TextColors.*
 import com.github.ajalt.mordant.rendering.TextStyle
+import com.github.ajalt.mordant.rendering.TextStyles.bold
 import com.github.ajalt.mordant.terminal.Terminal
-import com.github.ajalt.mordant.terminal.TextColors.*
-import com.github.ajalt.mordant.terminal.TextStyles.bold
-import com.github.ajalt.mordant.terminal.TextStyles.underline
 
 val terminal = Terminal()
 
-fun log(message: Any?, prefix: String = "") {
+fun log(prefix: String, message: Any?, messageColor: Color? = null) {
     terminal.println(
-        ("$prefix ${
-            terminal.renderMuted("(${Thread.currentThread().name}) >>")
-        } $message")
+        "$prefix ${
+            terminal.theme.muted("(${Thread.currentThread().name}) >>")
+        } ${TextStyle(messageColor, bold = true).invoke(message.toString())}"
     )
 }
 
-private fun renderMessage(message: Any?, color: Color? = null) =
-    terminal.render(message.toString(), TextStyle(color, bold = true))
+fun logInfo(message: Any?) = log(bold("INFO"), message)
 
-fun logInfo(message: Any?) =
-    log(renderMessage(message), terminal.render("INFO", bold.style))
+fun logMajorInfo(message: Any?) = log(magenta(bold("INFO")), message)
 
-fun logMajorInfo(message: Any?) =
-    log(renderMessage(message), terminal.render("INFO", TextStyle(magenta, bold = true)))
+fun logSuccess(message: Any?) = log(brightGreen(bold("INFO")), message)
 
-fun logSuccess(message: Any?) =
-    log(renderMessage(message), terminal.render("INFO", TextStyle(brightGreen, bold = true)))
+fun logWarning(message: Any?) = log(yellow(bold("WARN")), message, yellow)
 
-fun logWarning(message: Any?) =
-    log(renderMessage(message, yellow), terminal.render("WARN", TextStyle(yellow, bold = true)))
-
-fun logError(message: Any?) =
-    log(renderMessage(message, red), terminal.render("ERROR", TextStyle(brightRed, bold = true)))
-
-fun Throwable.logThis(severe: Boolean = true) {
-    val string = stackTraceToString()
-    val message = string.lines().first()
-    val splitMessage = message.split(": ")
-    val formattedMessage =
-        underline(splitMessage.first().removePrefix("java.lang.")) + ": ${
-            splitMessage.takeLast(splitMessage.lastIndex).joinToString(": ")
-        }"
-    val finalMessage = "$formattedMessage${terminal.renderMarkdown(string.removePrefix(message))}"
-    if (severe) logError(finalMessage) else logWarning(finalMessage)
-}
+fun logError(message: Any?) = log(brightRed(bold("ERROR")), message, red)
